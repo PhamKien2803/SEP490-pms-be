430bd8a4cb43acbbb71aff2f9a4c392b666254d6
PhamKien28 <134904000+PhamKien2803@users.noreply.github.com>
2025-10-01 23:52:14 +0700

    fix cors

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 // Khai báo app
 const app = express();
 // Middleware
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true, }));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(morgan("dev"));

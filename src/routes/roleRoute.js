fa559e71800e732ec95c73032350a10663849a18
Hieubthe173123 <trunghieubui866@gmail.com>
2025-10-02 23:35:20 +0700

    add verifyToken

--- src/routes/roleRoute.js
+++ src/routes/roleRoute.js
@@ -4,9 +4,29 @@ const Role = require("../models/roleModel");
 const { verifyToken, authorizeAction } = require("../middlewares/auth.middleware");
 const { createGeneric, deletedSoftGeneric, findAllGeneric, updateGeneric } = require("../controller/useController");
 
-router.get("/list", findAllGeneric(Role));
-router.post("/create", createGeneric(Role));
-router.put("/update/:id", updateGeneric(Role));
-router.post("/delete/:id", deletedSoftGeneric(Role));
+router.get("/list",
+    verifyToken,
+    authorizeAction("view"),
+    findAllGeneric(Role)
+);
+
+router.post("/create",
+    verifyToken,
+    authorizeAction("create"),
+    createGeneric(Role)
+);
+
+router.put("/update/:id",
+    verifyToken,
+    authorizeAction("update"),
+    updateGeneric(Role)
+);
+
+router.post("/delete/:id",
+    verifyToken,
+    authorizeAction("delete"),
+    deletedSoftGeneric(Role)
+);
 
 module.exports = router;
+

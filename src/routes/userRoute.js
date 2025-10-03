da341b3efcb394615b56d025e6094e13e58140f3
dev.ttcn <dev.ttcn@gmail.com>
2025-10-03 07:54:53 +0700

    feat6/accounts-management-be/hieudd

---
+++ src/routes/userRoute.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/userModel");
+const { verifyToken, authorizeAction } = require("../middlewares/auth.middleware");
+const { findAllGeneric, createGeneric, deletedSoftGeneric, updateGeneric } = require('../controller/useController');
+
+router.get(
+    "/list",
+    verifyToken,
+    authorizeAction("view"),
+    findAllGeneric(User)
+);
+
+router.post(
+    "/create",
+    verifyToken,
+    authorizeAction("create"),
+    createGeneric(User)
+);
+
+router.delete(
+    "/delete/:id",
+    verifyToken,
+    authorizeAction("delete"),
+    deletedSoftGeneric(User)
+);
+
+router.put(
+    "/update/:id",
+    verifyToken,
+    authorizeAction("update"),
+    updateGeneric(User)
+);
+
+module.exports = router;

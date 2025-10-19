344c8681c2ad5c0174003f924ecb6fa7523b0e93
Hieubthe173123 <trunghieubui866@gmail.com>
2025-10-19 23:48:18 +0700

    declare api event

--- src/controller/useController.js
+++ src/controller/useController.js
@@ -5,41 +5,41 @@ const { SEQUENCE_CODE } = require('../constants/useConstants');
 const i18n = require("../middlewares/i18n.middelware");
 
 const findAllGeneric = (Model) => async (req, res) => {
-    try {
-        let { limit, page } = req.query;
+  try {
+    let { limit, page } = req.query;
 
-        limit = parseInt(limit) || 30;
-        page = parseInt(page) || 1;
+    limit = parseInt(limit) || 30;
+    page = parseInt(page) || 1;
 
-        const offset = (page - 1) * limit;
+    const offset = (page - 1) * limit;
 
-        const queryString = {
-            active: { $eq: true },
-        };
+    const queryString = {
+      active: { $eq: true },
+    };
 
-        const totalCount = await Model.countDocuments(queryString);
+    const totalCount = await Model.countDocuments(queryString);
 
-        const data = await Model.find(queryString)
-            .skip(offset)
-            .limit(limit);
+    const data = await Model.find(queryString)
+      .skip(offset)
+      .limit(limit);
 
-        if (!data || data.length === 0) {
-            return res
-                .status(HTTP_STATUS.BAD_REQUEST)
-                .json("Không tìm thấy dữ liệu");
-        }
-
-        return res.status(HTTP_STATUS.OK).json({
-            data,
-            page: {
-                totalCount,
-                limit,
-                page,
-            },
-        });
-    } catch (error) {
-        return res.status(HTTP_STATUS.SERVER_ERROR).json(error);
+    if (!data || data.length === 0) {
+      return res
+        .status(HTTP_STATUS.BAD_REQUEST)
+        .json("Không tìm thấy dữ liệu");
     }
+
+    return res.status(HTTP_STATUS.OK).json({
+      data,
+      page: {
+        totalCount,
+        limit,
+        page,
+      },
+    });
+  } catch (error) {
+    return res.status(HTTP_STATUS.SERVER_ERROR).json(error);
+  }
 };
 
 const createGeneric = (Model) => async (req, res) => {
@@ -116,57 +116,71 @@ const createGeneric = (Model) => async (req, res) => {
 };
 
 const deletedSoftGeneric = (Model) => async (req, res) => {
-    try {
-        const data = await Model.findById(req.params.id);
-        if (!data) {
-            return res.status(HTTP_STATUS.NOT_FOUND).json(RESPONSE_MESSAGE.NOT_FOUND);
-        }
-        data.active = false;
-        await data.save();
-        return res.status(HTTP_STATUS.OK).json(RESPONSE_MESSAGE.DELETED);
-    } catch (err) {
-        res.status(HTTP_STATUS.SERVER_ERROR).json({ message: err.message });
+  try {
+    const data = await Model.findById(req.params.id);
+    if (!data) {
+      return res.status(HTTP_STATUS.NOT_FOUND).json(RESPONSE_MESSAGE.NOT_FOUND);
     }
+    data.active = false;
+    await data.save();
+    return res.status(HTTP_STATUS.OK).json(RESPONSE_MESSAGE.DELETED);
+  } catch (err) {
+    res.status(HTTP_STATUS.SERVER_ERROR).json({ message: err.message });
+  }
 }
 
 const updateGeneric = (Model) => async (req, res) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const data = await Model.findById(id);
-        if (!data) {
-            return res.status(HTTP_STATUS.BAD_REQUEST).json(RESPONSE_MESSAGE.NOT_FOUND);
-        }
-        Object.assign(data, req.body);
-
-        const uniqueFields = Object.keys(Model.schema.paths).filter(
-            key => Model.schema.paths[key].options.unique
-        );
-
-        for (const field of uniqueFields) {
-            const exists = await Model.findOne({ [field]: data[field], _id: { $ne: id } });
-            if (exists) {
-                return res.status(400).json({ message: `Trường ${field} đã tồn tại.` });
-            }
-        }
-        await data.save();
-
-        return res.status(HTTP_STATUS.UPDATED).json(RESPONSE_MESSAGE.UPDATED);
-    } catch (error) {
-        console.log("error updateGeneric", error);
-
-        if (error.name === "ValidationError") {
-            const messages = Object.values(error.errors).map(e => e.message);
-            return res.status(400).json({ message: messages.join(", ") });
-        }
-        res.status(HTTP_STATUS.SERVER_ERROR).json({ message: error.message });
+    const data = await Model.findById(id);
+    if (!data) {
+      return res.status(HTTP_STATUS.BAD_REQUEST).json(RESPONSE_MESSAGE.NOT_FOUND);
     }
+    Object.assign(data, req.body);
+
+    const uniqueFields = Object.keys(Model.schema.paths).filter(
+      key => Model.schema.paths[key].options.unique
+    );
+
+    for (const field of uniqueFields) {
+      const exists = await Model.findOne({ [field]: data[field], _id: { $ne: id } });
+      if (exists) {
+        return res.status(400).json({ message: `Trường ${field} đã tồn tại.` });
+      }
+    }
+    await data.save();
+
+    return res.status(HTTP_STATUS.UPDATED).json(RESPONSE_MESSAGE.UPDATED);
+  } catch (error) {
+    console.log("error updateGeneric", error);
+
+    if (error.name === "ValidationError") {
+      const messages = Object.values(error.errors).map(e => e.message);
+      return res.status(400).json({ message: messages.join(", ") });
+    }
+    res.status(HTTP_STATUS.SERVER_ERROR).json({ message: error.message });
+  }
 };
+
+const getByIdGeneric = (Model) => async (req, res) => {
+  try {
+    const data = await Model.findById(req.params.id);
+    if (!data) {
+      return res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Không tìm thấy dữ liệu" })
+    }
+    return res.status(HTTP_STATUS.OK).json(data);
+  } catch (error) {
+    console.log("error getByIdGeneric", error);
+    return res.status(HTTP_STATUS.SERVER_ERROR).json(error);
+  }
+}
 
 
 module.exports = {
-    findAllGeneric,
-    createGeneric,
-    updateGeneric,
-    deletedSoftGeneric
+  findAllGeneric,
+  createGeneric,
+  updateGeneric,
+  deletedSoftGeneric,
+  getByIdGeneric
 };

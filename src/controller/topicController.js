73e254cdec859999fd73185b1761a1b5d59f12bb
Hieubthe173123 <trunghieubui866@gmail.com>
2025-10-22 01:43:23 +0700

    fix endpoint list and getById

--- src/controller/topicController.js
+++ src/controller/topicController.js
@@ -7,6 +7,7 @@ const i18n = require("../middlewares/i18n.middelware");
 const Activity = require('../models/activityModel');
 const Event = require('../models/eventModel');
 const SchoolYear = require('../models/schoolYearModel');
+const Topic = require('../models/topicModel');
 const { HTTP_STATUS, RESPONSE_MESSAGE, USER_ROLES, VALIDATION_CONSTANTS } = require('../constants/useConstants');
 const { IMAP_CONFIG, SMTP_CONFIG } = require('../constants/mailConstants');
 const { sequencePattern } = require('../helpers/useHelpers');
@@ -75,5 +76,87 @@ exports.getAvailableActivityController = async (req, res) => {
     } catch (error) {
         console.error("Error getAvailableActivityController:", error);
         return res.status(500).json(error);
+    }
+};
+
+
+exports.getListController = async (req, res) => {
+    try {
+        let { limit, page, schoolYear } = req.query;
+        const dataSchoolYear = await SchoolYear.findOne({
+            active: { $eq: true },
+            schoolYear: schoolYear
+        })
+        console.log("[Bthieu] ~ dataSchoolYear:", dataSchoolYear);
+
+        limit = parseInt(limit) || 30;
+        page = parseInt(page) || 1;
+
+        const offset = (page - 1) * limit;
+
+        let queryString = {
+            active: { $eq: true },
+        }
+        if (schoolYear) {
+            queryString = {
+                active: { $eq: true },
+                schoolYear: dataSchoolYear._id
+            };
+        }
+        const totalCount = await Topic.countDocuments(queryString);
+
+        const data = await Topic.find(queryString, {
+            topicCode: 1,
+            topicName: 1,
+            month: 1,
+            age: 1,
+            createdBy: 1,
+            updatedBy: 1,
+            createdAt: 1,
+            updatedAt: 1
+        })
+            .skip(offset)
+            .limit(limit);
+
+        if (!data || data.length === 0) {
+            return res
+                .status(HTTP_STATUS.BAD_REQUEST)
+                .json("Không tìm thấy dữ liệu");
+        }
+
+        return res.status(HTTP_STATUS.OK).json({
+            data,
+            page: {
+                totalCount,
+                limit,
+                page,
+            },
+        });
+    } catch (error) {
+        return res.status(HTTP_STATUS.SERVER_ERROR).json(error);
+    }
+}
+
+exports.getByIdController = async (req, res) => {
+    try {
+        const dataTopic = await Topic.findById(req.params.id)
+            .populate('activitiFix.activity')
+            .populate('activitiCore.activity')
+            .populate('activitiEvent.activity');
+
+        if (!dataTopic) {
+            return res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Không tìm thấy dữ liệu chủ đề" });
+        }
+
+        const activitiesList = [
+            ...dataTopic.activitiFix.map(a => ({ ...a.activity.toObject(), sessionsPerWeek: a.sessionsPerWeek, type: 'Fix' })),
+            ...dataTopic.activitiCore.map(a => ({ ...a.activity.toObject(), sessionsPerWeek: a.sessionsPerWeek, type: 'Core' })),
+            ...dataTopic.activitiEvent.map(a => ({ ...a.activity.toObject(), sessionsPerWeek: a.sessionsPerWeek, type: 'Event' })),
+        ];
+
+        return res.status(HTTP_STATUS.OK).json(dataTopic);
+    } catch (error) {
+        console.log("Error getByIdController", error);
+        return res.status(HTTP_STATUS.SERVER_ERROR).json(error);
     }
 };

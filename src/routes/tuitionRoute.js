d593b9d49d5c7d085942549a36b69c36196cf235
Hieubthe173123 <trunghieubui866@gmail.com>
2025-11-08 10:38:19 +0700

    balance

--- src/routes/tuitionRoute.js
+++ src/routes/tuitionRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { verifyToken, authorizeAction } = require("../middlewares/auth.middleware");
 const { findAllGeneric, createGeneric, deletedSoftGeneric, updateGeneric } = require('../controller/useController');
-const { getDetailTuitionController, createTuitionPayment, handlePayOSWebhook, checkTuitionPaymentStatus } = require("../controller/tuitionController");
+const { getDetailTuitionController, createTuitionPayment, handlePayOSWebhook, checkTuitionPaymentStatus, getHistoryFeeController } = require("../controller/tuitionController");
 
 router.get(
     "/detail/:parentId",
@@ -21,6 +21,13 @@ router.post(
 router.post(
     "/web-hook",
     handlePayOSWebhook
+);
+
+router.get(
+    "/historyFees",
+    verifyToken,
+    authorizeAction("view"),
+    getHistoryFeeController
 )
 
 router.get("/check-status/:orderCode", checkTuitionPaymentStatus)
